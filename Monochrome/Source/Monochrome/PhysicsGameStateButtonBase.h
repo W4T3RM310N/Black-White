8915f32d8cf95d281c383260a2ab9828aa7cd454
Anthony Dalessandro <a.dalessandro@live.com>
2020-06-13 11:31:29 -0400

    finsiheed alternating background and phys buttpon.

---
+++ Monochrome/Source/Monochrome/PhysicsGameStateButtonBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameStateButtonBase.h"
+#include "PhysicsGameStateButtonBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MONOCHROME_API APhysicsGameStateButtonBase : public AGameStateButtonBase
+{
+	GENERATED_BODY()
+
+public:
+
+	APhysicsGameStateButtonBase(); 
+};
